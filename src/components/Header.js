1d95670d2a1a694b7beb3f1a24af178805c96f07
Ray <ray.dev@outlook.com>
2023-10-17 00:38:07 -0500

    c2 initial wireframes

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Header() {
+  return (
+<>
+        <div>Header Component</div>
+        <Link href="/">Home Page</Link>
+        
+    </>
+  )
+}
